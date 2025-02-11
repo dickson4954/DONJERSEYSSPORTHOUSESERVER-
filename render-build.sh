2e976fd890ea7dfec9290ec00368d5b85dbaf4ca
Dickson Murithi <dicksonmurithi155@gmail.com>
2025-02-12 00:47:50 +0300

    Fix missing dependencies for reportlab

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Update package list and install required dependencies
 apt-get update && apt-get install -y \
     libjpeg-dev \
     zlib1g-dev \
@@ -8,16 +6,6 @@ apt-get update && apt-get install -y \
     liblcms2-dev \
     libopenjp2-7-dev \
     libharfbuzz-dev \
-    libfribidi-dev \
-    libtiff5-dev \
-    python3-dev \
-    python3-pip \
-    build-essential \
-    gcc \
-    pkg-config
+    libfribidi-dev
 
-# Ensure pip is up-to-date
-pip install --upgrade pip
-
-# Install Python dependencies
 pip install -r requirements.txt
