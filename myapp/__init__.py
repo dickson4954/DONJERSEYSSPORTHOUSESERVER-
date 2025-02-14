6102a9c68c6d58b03dec834f98d7b2beb02e27f8
Dickson Murithi <dicksonmurithi155@gmail.com>
2025-02-14 18:09:01 +0300

    Updated CORS settings to allow GitHub Pages

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -15,7 +15,7 @@ def create_app():
     migrate.init_app(app, db)
     
     # Configure CORS to allow requests from frontend
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://dickson4954.github.io"]}},
+    CORS(app, resources={r"/*": {"origins": [ "https://dickson4954.github.io"]}},
      methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
      allow_headers=["Content-Type", "Authorization"],
      supports_credentials=True)
